d62122de4a7a959034b600338fe4aa9214ceea2d
neeiraaaa <neeiraaaa@gmail.com>
2021-10-14 00:31:52 +0300

    Changes

--- src/main/java/tasks/homework/cycletask/CycleArrayMin.java
+++ src/main/java/tasks/homework/cycletask/CycleArrayMin.java
@@ -2,17 +2,15 @@ package tasks.homework.cycletask;
 
 import java.util.Random;
 
-public class CycleArrayMin extends CycleArray {
+public class CycleArrayMin {
 
-    public int[] randomArray() {
+    public void randomArray() {
 
-        int[] myFirstArray = new CycleArrayMin().randomArray();
-        Random random = new Random();
+        int[] myFirstArray = new CycleArray().randomArray();
+
         int min = myFirstArray[0];
 
         for (int i = 1; i < myFirstArray.length; i++) {
-
-            myFirstArray[i] = random.nextInt(100);
 
             if (myFirstArray[i] < min) {
                 min = myFirstArray[i];
@@ -20,6 +18,5 @@ public class CycleArrayMin extends CycleArray {
             System.out.println(min);  //найти минимальный элемент массива и вывести результат в консоль
 
         }
-        return myFirstArray;
     }
 }
