030ed2cbb1f642fdb911046041b95018fc99a285
neeiraaaa <neeiraaaa@gmail.com>
2021-10-15 21:32:17 +0300

    bubbles homework

---
+++ src/main/java/project/bubbles/Bottle.java
@@ -0,0 +1,11 @@
+package project.bubbles;
+
+public class Bottle {
+
+    private int bottleValue;
+    public String bottleWater;
+
+    public void open() {
+
+    }
+}
---
+++ src/main/java/project/bubbles/Runner.java
@@ -0,0 +1,9 @@
+package project.bubbles;
+
+public class Runner {
+    public static void main(String[] args) {
+        float bottle1 = 0.5F;
+        float bottle2 = 1F;
+        float bottle3 = 1.5F;
+    }
+}
