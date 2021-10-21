d52398607dc7d7948f84eb0a158731e96389c3bb
neeiraaaa <neeiraaaa@gmail.com>
2021-10-22 01:47:46 +0300

    complete subtask NewNumbers

--- src/main/java/tasks/stringTask/NewNumbers.java
+++ src/main/java/tasks/stringTask/NewNumbers.java
@@ -8,18 +8,15 @@ public class NewNumbers {
 
     public static void main(String[] args) {
 
-        String textMixString= "Привет Ира 1, привет Настя 2, привет Оля 3";
+        String textMixString = "Привет Ира 1, привет Настя 2, привет Оля 3";
 
-        for(int i=0; i<textMixString.length(); i++)
-        {
-            //Метод Character.isDigit() — определяет, является ли указанное значение типа char цифрой.
-            //Метод charAt() — возвращает символ, расположенный по указанному индексу строки.
-            if(Character.isDigit(textMixString.charAt(i)))
-                System.out.print(textMixString.charAt(i) + "  ");
-
-
-            int[] array = new int[i];
-            System.out.println(Arrays.toString(array));
+        String[] parts = textMixString.split("\\D");
+        for (String x : parts) {
+            if (!x.equals("")) {
+                System.out.println(x);
+            }
         }
     }
 }
+
+
