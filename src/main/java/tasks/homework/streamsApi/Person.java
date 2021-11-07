083afc49e2bfe8b28f74282c295a4799dfc04876
neeiraaaa <neeiraaaa@gmail.com>
2021-11-07 20:07:16 +0300

    I finished task Person

--- src/main/java/tasks/homework/streamsApi/Person.java
+++ src/main/java/tasks/homework/streamsApi/Person.java
@@ -1,9 +1,8 @@
 package tasks.homework.streamsApi;
 
-//создать класс Person, с полями name, surname, age. Сгенерировать группу из 100 человек в возрасте от 15 до 30 лет.
-// Написать одну непрерывную цепочку stream вызовов, которая выбирает людей, возраст которых меньше 21,
-// выводит их имена и возраст в консоль, сортирует по фамилии, а потом по имени(использовать thenComparing у объекта Comparator),
-// берет 4 первых человека, формирует коллекцию из имен объектов, собирает все в коллекцию
+//создать класс Person, с полями name, surname, age
+
+import java.util.Objects;
 
 public class Person {
 
@@ -11,4 +10,55 @@ public class Person {
     private String name;
     private String surname;
 
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && Objects.equals(name, person.name) && Objects.equals(surname, person.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(age, name, surname);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                '}';
+    }
 }
